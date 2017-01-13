6c0caa252cbb3bdf22af1d60bf99c52d54c11528
Conrad-N <conradnovakovic@hotmail.com>
2017-01-13 14:07:42 -0500

    Added some infrastructure to "Structure" class to have light dynamics later.

--- src/pkgfinal/project/Structure.java
+++ src/pkgfinal/project/Structure.java
@@ -2,6 +2,7 @@ package pkgfinal.project;
 
 import DLibX.DConsole;
 import java.awt.Color;
+import java.awt.geom.Rectangle2D;
 
 public class Structure {
 
@@ -9,12 +10,14 @@ public class Structure {
     private double width;
     private double x;
     private double y;
+    private Rectangle2D rect;
 
     public Structure(double x, double y, double width, double height) {
         this.height = height;
         this.width = width;
         this.x = x;
         this.y = y;
+        this.rect = new Rectangle2D.Double(x, y, width, height);
     }
 
     public double getX() {
@@ -31,6 +34,10 @@ public class Structure {
 
     public double getHeight() {
         return height;
+    }
+    
+    public Rectangle2D getRect() {
+        return this.rect;
     }
 
     public void setX(double i) {
